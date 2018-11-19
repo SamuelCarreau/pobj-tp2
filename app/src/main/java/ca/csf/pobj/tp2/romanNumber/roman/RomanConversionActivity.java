69b74759d92a4a479d03cbe216e35193771e7e9b
samuel <samuelcarreau@gmail.com>
2018-11-19 15:15:39 -0500

    solve AndroidManifest probleme and now the keyboard is hiding when we it the convert button

--- app/src/main/java/ca/csf/pobj/tp2/romanNumber/roman/RomanConversionActivity.java
+++ app/src/main/java/ca/csf/pobj/tp2/romanNumber/roman/RomanConversionActivity.java
@@ -6,9 +6,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
-
+import android.app.Activity;
 
 import ca.csf.pobj.tp2.romanNumber.util.*;
 import ca.csf.pobj.tp2.R;
@@ -44,6 +45,7 @@ public class RomanConversionActivity extends AppCompatActivity {
 
     public void onConvertButtonClicked(View view) {
         doConvertAction();
+        hideKeyboard(this);
     }
 
     private void setListener(final EditText editText) {
@@ -108,6 +110,18 @@ public class RomanConversionActivity extends AppCompatActivity {
     private void showErrorMessage(String message) {
         Snackbar.make(findViewById(R.id.romanCoordinatorLayout), message, Snackbar.LENGTH_LONG)
                 .show();
+    }
+
+    public static void hideKeyboard(Activity activity) {
+        //Source : https://stackoverflow.com/questions/1109022/close-hide-the-android-soft-keyboard
+        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        //Find the currently focused view, so we can grab the correct window token from it.
+        View view = activity.getCurrentFocus();
+        //If no view currently has focus, create a new one, just so we can grab a window token from it
+        if (view == null) {
+            view = new View(activity);
+        }
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 
 
